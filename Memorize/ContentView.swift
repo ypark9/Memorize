998c62ebd9a1bcbf36da9aecce27fc7d24d3ba6a
ypark9@ncsu.edu <ypark9@ncsu.edu>
2020-07-14 18:02:02 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Yoonsoo Park on 7/14/20.
+//  Copyright © 2020 Yoonsoo Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
